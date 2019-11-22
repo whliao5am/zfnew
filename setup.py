c5669a9a36681abdab07e51f4d42ae397894704e
廖文豪 <18180257205@163.com>
2019-11-23 00:21:15 +0800

    update;

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='zfnew',
-    version='0.0.1',
+    version='0.0.3',
     author='NeroAsmar',
     author_email='neroasmarr@gmail.com',
     url='https://neroasmar.top/zfnew/',
@@ -13,17 +13,14 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
-    install_requires=['requests', 'bs4', 'rsa' 'binascii', 'Crypto', 'base64'],
+    install_requires=['requests', 'bs4', 'rsa', 'Crypto'],
     # extras_require={  # 分组依赖模块，可使用pip install sampleproject[dev] 安装分组内的依赖
     #    'dev': ['check-manifest'],
     #    'test': ['coverage'],
     # },
     python_requires='>=3.0.0',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',  # 当前开发进度等级（测试版，正式版等）
-
         'Intended Audience :: Developers',  # 模块适用人群
-        'Topic :: Software Development :: Build Tools',  # 给模块加话题标签
 
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',  # 模块的license
 
